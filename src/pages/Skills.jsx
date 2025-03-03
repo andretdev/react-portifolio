035bb39b7c3689fa2a58c5266502f63083ec37fc
Andre Tiago <andretiago368@gmail.com>
2025-03-03 11:08:12 -0300

    Created a card skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,12 +4,20 @@ function Skills() {
       id="skills"
       className="flex min-h-screen w-full flex-col justify-center gap-5 border-2 border-red-500 p-5 xl:flex-row"
     >
-      <div className="animate-appear-left w-100 h-100 border-1 inset-shadow-sm inset-shadow-purple-900 flex flex-col items-center justify-center rounded-lg border-purple-500 bg-blue-500 transition-all duration-500 ease-out hover:scale-[1.03]">
-        <p className="font-serif">Hello everyone this is a card compononent </p>
-        <p className="text-black">Tesst to github push</p>
-        <button className="mt-2 rounded-lg border-2 border-red-500 bg-blue-700 px-3 py-2 font-bold text-black transition-all duration-500 ease-out hover:scale-[1.03]">
-          Click
-        </button>
+      <div className="animate-appear-left flex w-full flex-col justify-center rounded-2xl border border-gray-400 px-10 shadow-lg xl:w-3/5">
+        <h3 className="font-stretch-semi-expanded flex justify-center text-5xl text-black">
+          Skills
+        </h3>
+
+        <ul className="text-black">
+          <li className="hover:bg-radial-[at_50%_75%] transtion from-sky-200 via-blue-400 to-indigo-900 to-90% duration-500 ease-out hover:bg-opacity-50">
+            HTML
+          </li>
+          <li className="">HTML</li>
+          <li className="">HTML</li>
+          <li className="">HTML</li>
+          <li className="">HTML</li>
+        </ul>
       </div>
     </section>
   );
