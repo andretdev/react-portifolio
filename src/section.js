5afbf2d33fd44c28dcbe48f37fff22bf441c2901
Andre Tiago <andretiago368@gmail.com>
2025-03-19 13:08:09 -0600

    Removed footer from sections

--- src/section.js
+++ src/section.js
@@ -2,7 +2,6 @@ const sections = [
   { id: "home", title: "Home" },
   { id: "aboutme", title: "About Me" },
   { id: "skills", title: "Skills" },
-  { id: "footer", title: "Footer" },
 ];
 
 export default sections;
